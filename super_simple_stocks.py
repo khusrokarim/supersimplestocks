8d14c63b64f2e92d18c392d482aaf51685480e08
Khusro Karim <khusro.karim@gmail.com>
2018-05-02 16:09:54 +0100

    Remaining functions for stock calculations

--- super_simple_stocks.py
+++ super_simple_stocks.py
@@ -1,3 +1,5 @@
+from collections import defaultdict, namedtuple
+from datetime import datetime, timedelta
 from decimal import Decimal
 
 def as_nonnegative_decimal(name, value, allow_none=False):
@@ -16,6 +18,17 @@ def as_nonnegative_decimal(name, value, allow_none=False):
     else:
         return Decimal(value)
 
+def validate_member(name, value, permitted):
+    if value not in permitted:
+        raise ValueError('{} must be one of {} (received {})'.format(name, permitted, value))
+    return value
+
+
+Trade = namedtuple('Trade', ['date_time', 'symbol', 'quantity', 'buy_or_sell', 'price'])
+
+class InvalidOperation(Exception):
+    pass
+
 
 class Stock:
     """A single stock, optionally traded on a single exchange."""
@@ -25,33 +38,100 @@ class Stock:
         'preferred',
     )
 
-    def __init__(self, symbol, stock_type, par_value, last_dividend=None, fixed_dividend=None):
+    def __init__(self, symbol, stock_type, par_value, last_dividend=Decimal(), fixed_dividend=None):
         self.symbol = symbol
-        self.stock_type = stock_type
+        self.stock_type = validate_member('stock_type', stock_type, self.stock_types)
         self.par_value = as_nonnegative_decimal('par_value', par_value)
-        self.last_dividend = as_nonnegative_decimal('last_dividend', last_dividend, True)
-        self.fixed_dividend = as_nonnegative_decimal('fixed_dividend', fixed_dividend, True)
-        self.exchange = None
+        self.last_dividend = last_dividend
+        self.fixed_dividend = fixed_dividend
+
+    def record_trade(self, quantity, buy_or_sell, price):
+        """Record a trade of this stock"""
+        self.exchange.record_trade(self.symbol, quantity, buy_or_sell, price)
+
+    def price(self, minutes=5):
+        """
+        Calculate and return the volume weighted stock price over the last 5 minutes.
+        If there were no trades in the last 5 minutes, return the price of the last trade.
+        If the stock has not been traded, return None.
+        """
+        now = datetime.utcnow()
+        calculate_since = now - timedelta(minutes=minutes)
+        all_trades = self.exchange.trades[self.symbol]
+
+        priced_trades = [
+            trade for trade in all_trades
+            if trade.date_time > calculate_since
+        ]
+
+        if not priced_trades:
+            try:
+                # No trades in the last 5 minutes
+                priced_trades = [all_trades[-1]]
+            except IndexError:
+                # No trades at all
+                return None
+
+        numerator = sum((trade.price * trade.quantity for trade in priced_trades))
+        denominator = sum((trade.quantity for trade in priced_trades))
+        return numerator/denominator
+
+    def dividend_yield(self):
+        """
+        Calculate and return the dividend yield of this stock.
+        If a common stock has no last_dividend, return 0.
+        Raises InvalidOperation if the stock has no price (because it has not been traded).
+        """
+        if not self.price():
+            raise InvalidOperation('Dividend yield for {} cannot be calculated because it has no price')
+        if self.stock_type == 'common':
+            return (self.last_dividend or 0)/ self.price()
+        else:
+            return (self.fixed_dividend * self.par_value) / self.price()
+
+    def pe_ratio(self):
+        """Calculate P/E ratio"""
+        # The given formula includes a variable 'dividend' without specifying fixed or last.
+        # Since the examples do not include fixed dividends for common stock, and since
+        # separate formulae are given for common and preferred where necessary (not for P/E ratio),
+        # I will assume that the last dividend is relevant here.
+        if not self.last_dividend:
+            raise InvalidOperation((
+                'P/E ratio cannot be calculated for {} '
+                'because it has not distributed a dividend'
+            ).format(self.symbol))
+        return self.price() / self.last_dividend
 
     @property
-    def stock_type(self):
-        return self._stock_type
+    def last_dividend(self):
+        return self._last_dividend
 
-    @stock_type.setter
-    def stock_type(self, value):
-        if value not in self.stock_types:
-            raise ValueError('stock_type must be one of {} (received {})'.format(self.stock_types, value))
-        self._stock_type = value
+    @last_dividend.setter
+    def last_dividend(self, value):
+        self._last_dividend = as_nonnegative_decimal('last_dividend', value)
 
     @property
     def fixed_dividend(self):
+        """Fixed dividend.  Required for preferred stocks."""
         return self._fixed_dividend
 
     @fixed_dividend.setter
     def fixed_dividend(self, value):
         if (self.stock_type == 'preferred') and (value is None):
             raise TypeError('Preferred stock must have a fixed dividend')
-        self._fixed_dividend = value
+        self._fixed_dividend = as_nonnegative_decimal('fixed_dividend', value, True)
+
+    @property
+    def exchange(self):
+        """The exchange associated with this stock"""
+        try:
+            return self._exchange
+        except AttributeError:
+            raise InvalidOperation("Stock {} is not associated with an exchange".format(self.symbol))
+
+    @exchange.setter
+    def exchange(self, value):
+        self._exchange = value
 
 
 class Exchange:
@@ -59,3 +139,22 @@ class Exchange:
 
     def __init__(self, name):
         self.name = name
+        self.stocks = {}
+        self.trades = defaultdict(list)
+
+    def add_stock(self, stock):
+        if stock.symbol in self.stocks:
+            raise InvalidOperation('Symbol {} already exists'.format(stock.symbol))
+        self.stocks[stock.symbol] = stock
+        stock.exchange = self
+
+    def record_trade(self, symbol, quantity, buy_or_sell, price):
+        # Validate values before creating a new key in self.trades
+        trade = Trade(
+            date_time=datetime.utcnow(),
+            symbol=validate_member('symbol', symbol, self.stocks),
+            quantity=as_nonnegative_decimal('quantity', quantity),
+            buy_or_sell=validate_member('buy_or_sell', buy_or_sell, ('buy', 'sell')),
+            price=as_nonnegative_decimal('price', price),
+        )
+        self.trades[symbol].append(trade)
--- tests/test_stock.py
+++ tests/test_stock.py
@@ -1,7 +1,8 @@
 import unittest
+from datetime import datetime, timedelta
 from decimal import Decimal
 
-from super_simple_stocks import Stock, Exchange
+from super_simple_stocks import Stock, Exchange, Trade, InvalidOperation
 
 
 class TestStock(unittest.TestCase):
@@ -16,20 +17,82 @@ class TestStock(unittest.TestCase):
         with self.assertRaises(ValueError):
             Stock('TEST', 'common', -5)
 
-    # def test_dividend_yield(self):
-    #     self.fail()
+    def test_dividend_yield(self):
+        exchange = Exchange('Test Exchange')
+        stock = Stock('TEST', 'common', 100, 10)
+        exchange.add_stock(stock)
+        stock.record_trade(10, 'buy', 2)
+        stock.record_trade(20, 'sell', 2)
+        stock.record_trade(30, 'buy', 3)
+        self.assertEqual(stock.dividend_yield(), 4)
+        stock = Stock('TESTPREFERRED', 'preferred', 100, 10, Decimal('0.25'))
+        exchange.add_stock(stock)
+        stock.record_trade(10, 'buy', 2)
+        stock.record_trade(20, 'sell', 2)
+        stock.record_trade(30, 'buy', 3)
+        self.assertEqual(stock.dividend_yield(), 10)
 
-    # def test_pe_ratio(self):
-    #     self.fail()
+    def test_pe_ratio(self):
+        exchange = Exchange('Test Exchange')
+        stock = Stock('TEST', 'common', 100, 10)
+        exchange.add_stock(stock)
+        stock.record_trade(10, 'buy', 2)
+        stock.record_trade(20, 'sell', 2)
+        stock.record_trade(30, 'buy', 3)
+        self.assertEqual(stock.pe_ratio(), Decimal('0.25'))
 
-    # def test_record_trade(self):
-    #     self.fail()
+    def test_price(self):
+        exchange = Exchange('Test Exchange')
+        stock = Stock('TEST', 'common', 100)
+        exchange.add_stock(stock)
+        self.assertIsNone(stock.price())
+        stock.record_trade(10, 'buy', 2)
+        stock.record_trade(20, 'sell', 2)
+        stock.record_trade(30, 'buy', 3)
+        self.assertEqual(stock.price(), Decimal('2.5'))
+        new_trades = []
+        for trade in exchange.trades[stock.symbol]:
+            six_minutes_ago = datetime.utcnow() - timedelta(minutes=6)
+            new_trades.append(
+                Trade(
+                    date_time=six_minutes_ago,
+                    symbol=trade.symbol,
+                    quantity=trade.quantity,
+                    buy_or_sell=trade.buy_or_sell,
+                    price=trade.price,
+                )
+            )
+        exchange.trades[stock.symbol] = new_trades
+        self.assertEqual(stock.price(), 3)
 
-    # def test_price(self):
-    #     self.fail()
+
 
 class TestExchange(unittest.TestCase):
 
+    def setUp(self):
+        self.exchange = Exchange('Test Exchange')
+
     def test_create_exchange(self):
-        exchange = Exchange('Test Exchange')
-        self.assertEqual(exchange.name, 'Test Exchange')
+        self.assertEqual(self.exchange.name, 'Test Exchange')
+
+    def test_add_stock(self):
+        stock = Stock('TEST', 'common', 100)
+        self.exchange.add_stock(stock)
+        self.assertEqual(self.exchange.stocks[stock.symbol], stock)
+
+    def test_record_trade(self):
+        stock = Stock('TEST', 'common', 100)
+        with self.assertRaises(InvalidOperation):
+            stock.record_trade(10, 'buy', 300)
+        self.exchange.add_stock(stock)
+        stock.record_trade(10, 'buy', 300)
+        trade = self.exchange.trades[stock.symbol][0]
+        self.assertEqual(trade.symbol, stock.symbol)
+        self.assertEqual(trade.quantity, 10)
+        self.assertEqual(trade.price, 300)
+        with self.assertRaises(ValueError):
+            stock.record_trade(-4, 'buy', 300)
+        with self.assertRaises(ValueError):
+            stock.record_trade(10, 'discard', 300)
+        with self.assertRaises(ValueError):
+            stock.record_trade(10, 'buy', -1)
